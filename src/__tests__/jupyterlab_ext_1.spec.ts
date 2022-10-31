1b4362b82479b04545390b58502208cd1ce5fbbd
Arthur Keen <aakeen@yahoo.com>
2022-10-31 13:24:20 -0600

    Seed apod project from cookiecutter

---
+++ src/__tests__/jupyterlab_ext_1.spec.ts
@@ -0,0 +1,9 @@
+/**
+ * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
+ */
+
+describe('jupyterlab_ext_1', () => {
+  it('should be tested', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
